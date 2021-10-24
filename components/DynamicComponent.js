2db80a81e5014c0b86f8faaa18e7e8d8c05daa93
Jeremy-Percy Batten <jeremy@jeremypercy.nl>
2021-10-24 16:59:18 +0200

    initial commit

---
+++ components/DynamicComponent.js
@@ -0,0 +1,27 @@
+import SbEditable from 'storyblok-react'
+import Teaser from './Teaser'
+import Grid from './Grid'
+import Feature from './Feature'
+import Page from './Page'
+
+// resolve Storyblok components to Next.js components
+const Components = {
+ 'teaser': Teaser,
+ 'grid': Grid,
+ 'feature': Feature,
+ 'page': Page,
+}
+
+const DynamicComponent = ({blok}) => {
+ // check if component is defined above
+ if (typeof Components[blok.component] !== 'undefined') {
+   const Component = Components[blok.component]
+   // wrap with SbEditable for visual editing
+   return (<SbEditable content={blok}><Component blok={blok} /></SbEditable>)
+ }
+ 
+ // fallback if the component doesn't exist
+ return (<p>The component <strong>{blok.component}</strong> has not been created yet.</p>)
+}
+
+export default DynamicComponent
---
+++ pages/index.tsx
@@ -0,0 +1,70 @@
+import type { GetStaticProps, NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import React from 'react';
+import DynamicComponent from '../components/DynamicComponent'
+import { StoriesParams, StoryData } from 'storyblok-js-client';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+font-size: 200px;
+`
+
+interface Props {
+  story: StoryData
+}
+
+// The Storyblok Client
+import Storyblok from "../lib/storyblok"
+ 
+export default function Home({story}: Props) {
+
+  console.log(story)
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+ 
+      <header>
+        <Title>
+          { story ? story.name : 'My Site' }
+        </Title>
+      </header>
+ 
+      <main>
+      <DynamicComponent blok={story.content} />
+      </main>
+    </div>
+  )
+}
+ 
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
+  // the slug of the story
+  let slug = "home"
+  // the storyblok params
+  let params: StoriesParams = {
+    version: "draft", // or 'published'
+  }
+ 
+  // checks if Next.js is in preview mode
+  if (context.preview) {
+    // loads the draft version
+    params.version = "draft"
+    // appends the cache version to get the latest content
+    params.cv = Date.now()
+  }
+ 
+  // loads the story from the Storyblok API
+  let { data } = await Storyblok.get(`cdn/stories/${slug}`, params)
+ 
+  // return the story from Storyblok and whether preview mode is active
+  return {
+    props: {
+      story: data ? data.story : false,
+      preview: context.preview || false
+    },
+    revalidate: 10, 
+  }
+}
